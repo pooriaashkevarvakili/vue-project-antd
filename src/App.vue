40d35ac18d49eb52c73f57a18308f9b6ed6485c2
pvakili <pooriavakili>
2021-03-27 17:04:58 +0430

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
+
+    <p class="text-3xl text-red-500 uppercase	">hello world</p>
+  <a-button class="text-2xl text-green-500 p-3" type="primary">
+    Primary
+  </a-button>
+
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
-
+<script>
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import { Button, message } from 'ant-design-vue';
+import router from "./router/index"
+import store from "./store/index"
+const app = createApp();
+app.config.productionTip = false;
 
-createApp(App).use(store).use(router).mount('#app')
+/* Automatically register components under Button, such as Button.Group */
+app.use(Button);
+app.use(router)
+app.use(store)
+
+
+app.config.globalProperties.$message = message;
