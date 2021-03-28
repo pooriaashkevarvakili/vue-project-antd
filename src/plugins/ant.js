79fe4478b4a10dc7e9f3fea6b741ce51c7da933f
pvakili <pooriavakili>
2021-03-28 10:08:01 +0430

    firstcommit

--- src/plugins/ant.js
+++ src/plugins/ant.js
@@ -1,11 +1,4 @@
 import { createApp } from 'vue';
-import { Button, message } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
  const ant = createApp();
-ant.config.productionTip = false;
-
-/* Automatically register components under Button, such as Button.Group */
-ant.use(Button);
-
-ant.config.globalProperties.$message = message;
 export default  ant;
